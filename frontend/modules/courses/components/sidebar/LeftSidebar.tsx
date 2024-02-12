b72ffb257c116dcd5fc22f8d37c61fdb4de73eaa
DragosGhinea <dragos.dev12@gmail.com>
2024-02-13 00:53:34 +0200

    LeftSideBar improvements

--- frontend/modules/courses/components/sidebar/LeftSidebar.tsx
+++ frontend/modules/courses/components/sidebar/LeftSidebar.tsx
@@ -1,11 +1,51 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import {
+  TooltipProvider,
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+} from "@/components/ui/tooltip";
+import { EyeIcon, SearchIcon, StarsIcon } from "lucide-react";
+import React, { ReactNode } from "react";
+
+const LeftSidebarItem = ({ name, icon }: { name: string; icon: ReactNode }) => {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={0}>
+        <TooltipTrigger asChild>
+          <Button
+            variant="outline"
+            className="inline-flex min-h-16 min-w-16 items-center gap-0 rounded-lg border-4 border-dark-300 @[16rem]:gap-4"
+          >
+            {icon}
+            <span className="base-semibold max-w-0 overflow-hidden text-nowrap transition-[max-width] duration-300 @[16rem]:max-w-[100%]">
+              {name}
+            </span>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent
+          side="right"
+          align="center"
+          sideOffset={5}
+          className="@[16rem]:hidden"
+        >
+          <p className="base-semibold">{name}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
 
 const LeftSidebar = () => {
-  return <ul className="min-w-[200px]">
-    <li>Search</li>
-    <li>Recently Visited</li>
-    <li>Recommended</li>
-  </ul>;
+  return (
+    <div className="flex size-full flex-col items-center @container">
+      <div className="base-semibold flex h-full flex-col justify-between gap-5 py-10">
+        <LeftSidebarItem name="Search Course" icon={<SearchIcon />} />
+        <LeftSidebarItem name="Recently Visited" icon={<EyeIcon />} />
+        <LeftSidebarItem name="Recommended" icon={<StarsIcon />} />
+      </div>
+    </div>
+  );
 };
 
 export default LeftSidebar;
--- frontend/modules/courses/pages/CoursesLayoutClient.tsx
+++ frontend/modules/courses/pages/CoursesLayoutClient.tsx
@@ -24,10 +24,16 @@ const CoursesLayoutClient = ({
     <main className="relative flex size-full flex-col bg-light-850 dark:bg-dark-300">
       <Navbar />
       <ResizablePanelGroup direction="horizontal" onLayout={onLayout}>
-        <ResizablePanel defaultSize={defaultLayout[0]} minSize={5}>
+        <ResizablePanel
+          defaultSize={defaultLayout[0]}
+          minSize={7}
+          maxSize={30}
+          style={{ overflow: "visible" }}
+        >
           <LeftSidebar />
         </ResizablePanel>
-        <ResizableHandle />
+        {/* static is required for tooltip to show above */}
+        <ResizableHandle className="static" />
         <ResizablePanel defaultSize={defaultLayout[1]}>
           {children}
         </ResizablePanel>
