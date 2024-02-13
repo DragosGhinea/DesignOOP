308cab3f4c39d405c01afc4ab076db954c6047ad
DragosGhinea <dragos.dev12@gmail.com>
2024-02-13 13:56:13 +0200

    Improvement on the animation of left sidebar

--- frontend/modules/courses/components/sidebar/LeftSidebar.tsx
+++ frontend/modules/courses/components/sidebar/LeftSidebar.tsx
@@ -1,48 +1,48 @@
 import { Button } from "@/components/ui/button";
 import {
-  TooltipProvider,
   Tooltip,
   TooltipTrigger,
   TooltipContent,
 } from "@/components/ui/tooltip";
+import { TooltipProvider } from "@radix-ui/react-tooltip";
 import { EyeIcon, SearchIcon, StarsIcon } from "lucide-react";
 import React, { ReactNode } from "react";
 
 const LeftSidebarItem = ({ name, icon }: { name: string; icon: ReactNode }) => {
   return (
-    <TooltipProvider>
-      <Tooltip delayDuration={0}>
-        <TooltipTrigger asChild>
-          <Button
-            variant="outline"
-            className="inline-flex min-h-16 min-w-16 items-center gap-0 rounded-lg border-4 border-dark-300 @[16rem]:gap-4"
-          >
-            {icon}
-            <span className="base-semibold max-w-0 overflow-hidden text-nowrap transition-[max-width] duration-300 @[16rem]:max-w-[100%]">
-              {name}
-            </span>
-          </Button>
-        </TooltipTrigger>
-        <TooltipContent
-          side="right"
-          align="center"
-          sideOffset={5}
-          className="@[16rem]:hidden"
+    <Tooltip delayDuration={0}>
+      <TooltipTrigger asChild>
+        <Button
+          variant="outline"
+          className="inline-flex min-h-16 min-w-16 max-w-16 items-center rounded-lg border-4 border-dark-300 transition-[max-width] duration-300 @[16rem]:max-w-[100%] @[16rem]:gap-4"
         >
-          <p className="base-semibold">{name}</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+          <div>{icon}</div>
+          <span className="base-semibold overflow-hidden text-nowrap ">
+            {name}
+          </span>
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent
+        side="right"
+        align="center"
+        sideOffset={5}
+        className="@[16rem]:hidden"
+      >
+        <p className="base-semibold">{name}</p>
+      </TooltipContent>
+    </Tooltip>
   );
 };
 
 const LeftSidebar = () => {
   return (
     <div className="flex size-full flex-col items-center @container">
-      <div className="base-semibold flex h-full flex-col justify-between gap-5 py-10">
-        <LeftSidebarItem name="Search Course" icon={<SearchIcon />} />
-        <LeftSidebarItem name="Recently Visited" icon={<EyeIcon />} />
-        <LeftSidebarItem name="Recommended" icon={<StarsIcon />} />
+      <div className="base-semibold flex h-full max-w-16 flex-col justify-between gap-5 py-10 transition-[max-width] duration-300 @[16rem]:max-w-[100%]">
+        <TooltipProvider>
+          <LeftSidebarItem name="Search Course" icon={<SearchIcon />} />
+          <LeftSidebarItem name="Recently Visited" icon={<EyeIcon />} />
+          <LeftSidebarItem name="Recommended" icon={<StarsIcon />} />
+        </TooltipProvider>
       </div>
     </div>
   );
