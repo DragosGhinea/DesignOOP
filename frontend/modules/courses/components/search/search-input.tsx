ecc5464ea09b2c5ad4235de8b3d5d9c766e388db
DragosGhinea <dragos.dev12@gmail.com>
2024-02-14 18:33:47 +0200

    Course search bar improvement

--- frontend/modules/courses/components/search/search-input.tsx
+++ frontend/modules/courses/components/search/search-input.tsx
@@ -1,18 +1,31 @@
+"use client";
+
 import { cn } from "@/lib/utils";
 import { SearchIcon } from "lucide-react";
 import React from "react";
+import { TypeAnimation } from "react-type-animation";
 
 const SearchInput = ({ className }: { className?: string }) => {
   return (
     <div className={cn("relative flex items-center", className)}>
       <input
-        type="text"
         placeholder="Search"
-        className="h6-typography peer w-full border-b-4 border-blue-200 py-1 pb-2 pl-12 text-blue-600 transition-colors placeholder:text-blue-400 focus:border-blue-500 focus:outline-none"
+        type="text"
+        className="h6-typography peer w-full border-b-4 border-blue-200 bg-transparent py-1 pl-12 text-blue-600 transition-colors placeholder:text-transparent focus:border-blue-500 focus:outline-none focus:placeholder:text-blue-400 dark:border-blue-400 dark:text-blue-200 dark:focus:border-blue-300 dark:focus:placeholder:text-blue-200"
         autoComplete="off"
       />
 
-      <div className="pointer-events-none absolute bottom-2 text-blue-400 peer-focus:text-blue-700">
+      <div className="h6-typography pointer-events-none invisible absolute bottom-2 left-12 text-blue-400 transition-all duration-300 peer-placeholder-shown:visible peer-focus:opacity-0 dark:text-blue-200">
+        Search:{" "}
+        <TypeAnimation
+          cursor={false}
+          sequence={["Singleton", 1000, "Observer Pattern", 1000]}
+          repeat={Infinity}
+          className="font-bold"
+        />
+      </div>
+
+      <div className="pointer-events-none absolute bottom-2 text-blue-400 peer-focus:text-blue-700 dark:peer-focus:text-blue-200">
         <SearchIcon className="size-8" />
       </div>
     </div>
