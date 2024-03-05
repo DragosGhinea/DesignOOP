eb343b21747ac4733303e295dacebd758e16f42e
DragosGhinea <dragos.dev12@gmail.com>
2024-03-06 00:19:47 +0200

    Made possible to add scrollbar class through scroll-area

--- frontend/components/ui/scroll-area.tsx
+++ frontend/components/ui/scroll-area.tsx
@@ -7,8 +7,8 @@ import { cn } from "@/lib/utils";
 
 const ScrollArea = React.forwardRef<
   React.ElementRef<typeof ScrollAreaPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root>
->(({ className, children, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof ScrollAreaPrimitive.Root> & {scrollbarClassName?: string}
+>(({ className, children, scrollbarClassName, ...props }, ref) => (
   <ScrollAreaPrimitive.Root
     ref={ref}
     className={cn("relative overflow-hidden", className)}
@@ -17,7 +17,7 @@ const ScrollArea = React.forwardRef<
     <ScrollAreaPrimitive.Viewport className="size-full rounded-[inherit]">
       {children}
     </ScrollAreaPrimitive.Viewport>
-    <ScrollBar />
+    <ScrollBar className={scrollbarClassName}/>
     <ScrollAreaPrimitive.Corner />
   </ScrollAreaPrimitive.Root>
 ));
--- frontend/modules/courses/components/sections/middle/course-editor.tsx
+++ frontend/modules/courses/components/sections/middle/course-editor.tsx
@@ -16,7 +16,10 @@ const CourseEditor = () => {
       </div>
       <Separator className="mt-3" />
 
-      <ScrollArea className="size-full flex-1">
+      <ScrollArea
+        className="size-full flex-1"
+        scrollbarClassName="dark:bg-dark-300"
+      >
         <JSONEditor />
       </ScrollArea>
     </div>
