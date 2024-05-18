4d5a89dffc4562f6456dd36f88c303a8a65dbdc4
DragosGhinea <dragos.dev12@gmail.com>
2024-05-18 19:50:28 +0300

    Improved middleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,47 +1,27 @@
-import { NextAuthMiddlewareOptions, withAuth } from "next-auth/middleware";
-import { computeCallbackURL } from "@/utils/common";
+import {
+  NextAuthMiddlewareOptions,
+  NextRequestWithAuth,
+  withAuth,
+} from "next-auth/middleware";
 import { options } from "@/modules/users/next_auth/auth.config";
-import { NextRequest } from "next/server";
 
-const DEFAULT_LOGIN_REDIRECT = "/";
-const apiAuthPrefix = "/api/auth";
-const authRoutes = ["/login"];
 const publicRoutes = [
   /^\/$/, // matching "/"
   /^\/courses(\/.*)?$/, // matching "/courses" and "/courses/**"
-  /^\/profile$/, // matching "/profile"
   /^\/test$/, // matching "/test"
 ];
+const adminRoutes = ["/admin"];
 
 export default withAuth(
-  (req: NextRequest) => {
-    // const { nextUrl } = req;
-    // const isLoggedIn = !!req.nextauth.token;
-    // console.log("TEST");
-    // const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-    // const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
-    // const isAuthRoute = authRoutes.includes(nextUrl.pathname);
-    // if (isApiAuthRoute) {
-    //   return null;
-    // }
-    // console.log("TEST ", isPublicRoute)
-    // if (isPublicRoute) {
-    //   return null;
-    // }
-    // // if (isAuthRoute) {
-    // //   if (isLoggedIn) {
-    // //     return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
-    // //   }
-    // //   return null;
-    // // }
-    // // if (!isLoggedIn && !isPublicRoute) {
-    // //   const encodedCallbackUrl = computeCallbackURL(nextUrl.pathname, nextUrl.search);
-    // //   return Response.redirect(new URL(
-    // //     `/login?callbackUrl=${encodedCallbackUrl}`,
-    // //     nextUrl
-    // //   ));
-    // // }
-    // return null;
+  (req: NextRequestWithAuth) => {
+    const isAdminRoute = adminRoutes.includes(req.nextUrl.pathname);
+    if (isAdminRoute) {
+      const isAdmin =
+        req.nextauth?.token!.user.authorities.includes("ROLE_ADMIN");
+      if (isAdmin) return null;
+
+      return Response.redirect(req.nextUrl.origin);
+    }
   },
   {
     pages: options.pages,
