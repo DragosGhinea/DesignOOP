8460fcfb30d88f46c705315d8f076a9b867b4354
DragosGhinea <dragos.dev12@gmail.com>
2024-07-01 00:13:37 +0300

    Small screen enforce vertical container

--- frontend/modules/courses/components/course/components/container.tsx
+++ frontend/modules/courses/components/course/components/container.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/utils/common";
 import React, { ReactNode } from "react";
+import { useMediaQuery } from "usehooks-ts";
 
 const Container = ({
   title,
@@ -12,12 +13,16 @@ const Container = ({
   children: ReactNode;
   id?: string;
 }) => {
+  const shouldEnforceVertical = useMediaQuery("(max-width: 540px)", {
+    defaultValue: false,
+  });
+
   return (
     <div className="flex flex-col gap-2">
       {title && (
         <h2 className="h3-typography text-center font-bold">{title}</h2>
       )}
-      <div className={cn("flex", vertical && "flex-col")} id={id}>
+      <div className={cn("flex", (vertical || shouldEnforceVertical) && "flex-col")} id={id}>
         {children}
       </div>
     </div>
